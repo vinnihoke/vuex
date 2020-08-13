16a4f89a5e6101ed8c301c48f68a37baf5f19b3a
Vinni Hoke <vinni.hoke@gmail.com>
2020-08-12 18:13:32 -0700

    adding state persistenc e with localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,24 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createCache from 'vuex-cache';
+import createPersistedState from "vuex-persistedstate";
 import products from './modules/products.module';
 import kanyeism from './modules/kanyeism.module';
 
 Vue.use(Vuex);
+
+const dataState = createPersistedState({
+	paths: ["products", "kanyeism"]
+})
 
 export default new Vuex.Store({
 	strict: true,
 	modules: {
 		products,
 		kanyeism
-	}
+	},
+	plugins: [
+		createCache(),
+		dataState
+	]
 })
