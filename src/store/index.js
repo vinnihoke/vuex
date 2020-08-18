2c7273aaafeb4da656d1b84c029cd9abbf9022df
Vinni Hoke <vinni.hoke@gmail.com>
2020-08-17 19:03:40 -0700

    Cleanup

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import createCache from 'vuex-cache';
 import createPersistedState from "vuex-persistedstate";
 import products from './modules/products.module';
 import kanyeism from './modules/kanyeism.module';
@@ -18,7 +17,6 @@ export default new Vuex.Store({
 		kanyeism
 	},
 	plugins: [
-		createCache(),
 		dataState
 	]
 })
